fb04567cd4836504529e0d2e23ef9aade763b080
Aryan Chaudhary <155959967+aryank77@users.noreply.github.com>
2026-08-19 23:38:57 +0530

    objects.js

---
+++ objects.js
@@ -0,0 +1,10 @@
+const Js_User = {
+    name: "Aryan",
+    age: 20,
+    city: "Delhi",
+    email: "aryan@example.com",
+    isActive: true,
+    hobbies: ["reading", "traveling", "coding"],
+    address: {Lucknow: "India", NewYork: "USA"},
+}
+console.log(Js_User.name) // output: Aryan  
